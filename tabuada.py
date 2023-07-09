9adc9b7dab39eb8317683db37e47ca410fbf8def
Paulo Roberto Tassinari Ignacio <72086403+paulortignacio@users.noreply.github.com>
2023-07-09 16:57:07 -0300

    Adicionando projeto ao controle de versão.

---
+++ tabuada.py
@@ -0,0 +1,35 @@
+import os
+import time
+
+def limpar():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+while True:
+    print("==============================================")
+    print("|       Programa de Cálculo de Tabuada       |")
+    print("==============================================")
+    try:
+        numero = int(input("Digite um número para calcular a tabuada: "))
+    except:
+        print("Digite um número válido!")
+        time.sleep(2)
+        continue
+    print(f"A tabuada do {numero} é:")
+    i = 1
+    while i <= 10:
+        print(f"{numero} x {i} = {numero * i}")
+        i += 1
+    saida = input("Deseja continuar? (S/N)").upper()
+    if saida == "N":
+        print("Encerrando o programa...")
+        time.sleep(1)
+        limpar()
+        break
+
+    else:
+        print("Reiniciando o programa...")
+        time.sleep(0.5)
+        limpar()
